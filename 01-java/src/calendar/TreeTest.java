d5e76146805ea940117796e40dfb3ad1bdd4d465
Pilna Barbora <st52947@upce.cz>
2021-01-08 08:52:00 +0100

    added blank tests

---
+++ 01-java/src/calendar/TreeTest.java
@@ -0,0 +1,30 @@
+package calendar;
+
+import static org.junit.Assert.*;
+
+public class TreeTest {
+
+    @org.junit.Test
+    public void iterator() {
+    }
+
+    @org.junit.Test
+    public void add() {
+    }
+
+    @org.junit.Test
+    public void erase() {
+    }
+
+    @org.junit.Test
+    public void contaix() {
+    }
+
+    @org.junit.Test
+    public void get() {
+    }
+
+    @org.junit.Test
+    public void testToString() {
+    }
+}
